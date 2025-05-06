f0ca44e8f09b19e8a2d8b64f93930e1ecc44f801
Kaveeshakavindi <kaveesha.20210431@iit.ac.lk>
2025-05-06 12:55:12 +0530

    login

--- src/components/ui/verticlenav.tsx
+++ src/components/ui/verticlenav.tsx
@@ -1,4 +1,4 @@
-import { Box, Tab, TabList, Tabs, Text } from '@chakra-ui/react'
+import { Box, Button, Tab, TabList, Tabs, Text } from '@chakra-ui/react'
 import Link from 'next/link'
 import { BiChevronRightSquare } from 'react-icons/bi'
 
@@ -13,27 +13,49 @@ export default function VerticleTabs () {
       height='100vh'
       position='fixed'
       zIndex='1000'
+      justifyContent='center'
     >
-      <TabList minW='250px' borderLeft='2px'>
+      <TabList
+        minW='250px'
+        borderLeft='2px'
+        display='flex'
+        flexDirection='column'
+        justifyContent='space-between'
+      >
+        <Box>
+          <Box
+            display='flex'
+            flexDirection='row'
+            alignItems='center'
+            gap='0.8rem'
+            width='100%'
+            justifyContent='center'
+            paddingY='2rem'
+          >
+            <BiChevronRightSquare color='var(--secondary)' size='1.5rem' />
+            <Text fontFamily='heading' color='white' fontSize='1.2rem'>
+              Next Gen
+            </Text>
+          </Box>
+          {pages.slice(0, 4).map((page, index) => (
+            <Tab
+              key={index}
+              as={Link}
+              href={page === 'home' ? '/' : `/${page}`}
+            >
+              {page.charAt(0).toUpperCase() + page.slice(1)}
+            </Tab>
+          ))}
+        </Box>
         <Box
           display='flex'
-          flexDirection='row'
-          alignItems='center'
-          gap='0.8rem'
-          width='100%'
           justifyContent='center'
-          paddingY='2rem'
+          width='100%'
+          mt='2rem'
+          mb='3rem'
         >
-          <BiChevronRightSquare color='var(--secondary)' size='1.5rem' />
-          <Text fontFamily='heading' color='white' fontSize='1.2rem'>
-            Next Gen
-          </Text>
+          <Button colorScheme='whiteAlpha'>Sign In</Button>
         </Box>
-        {pages.slice(0, 4).map((page, index) => (
-          <Tab key={index} as={Link} href={page === 'home' ? '/' : `/${page}`}>
-            {page.charAt(0).toUpperCase() + page.slice(1)}
-          </Tab>
-        ))}
       </TabList>
     </Tabs>
   )
